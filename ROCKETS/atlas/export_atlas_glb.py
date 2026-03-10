d411dd10802a671c6390f327a3aedb6ecb92e962
Gonzalo <demagureguisanchezgonzalo@gmail.com>
2026-03-10 22:57:44 +0100

    blend_view

---
+++ ROCKETS/atlas/export_atlas_glb.py
@@ -0,0 +1,16 @@
+﻿import bpy
+import os
+
+out_path = r"c:\\Users\\demag\\OneDrive - UPV EHU\\Informatica\\VS Code\\BISKY\\ByM\\ROCKETS\\atlas\\atlas.glb"
+os.makedirs(os.path.dirname(out_path), exist_ok=True)
+
+bpy.ops.export_scene.gltf(
+    filepath=out_path,
+    export_format='GLB',
+    export_apply=True,
+    export_texcoords=True,
+    export_normals=True,
+    export_materials='EXPORT',
+    export_yup=True
+)
+print('EXPORTED:', out_path)
